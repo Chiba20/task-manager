b8783ec8356b9426716131e233180d15d691265a
Chiba20 <zda23b016@iitmz.ac.in>
2025-11-11 18:34:07 +0300

    Update student_info.py

--- student_info.py
+++ student_info.py
@@ -1,11 +1,6 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
-# --------------------------
-# Mock student database (mock data)
-# Keys are strings to match query like ?id=12345
-# --------------------------
 students = {
     "12345": {"name": "Amina Omar", "course": "Computer Science", "year": 3, "gpa": 3.6},
     "23456": {"name": "Chiba Issa",   "course": "Information Systems", "year": 2, "gpa": 3.2},
@@ -13,10 +8,8 @@ students = {
     "45678": {"name": "Ali Hassan", "course": "Engineering", "year": 4, "gpa": 3.1}
 }
 
-# ------------------------------------------------------------
-# /api/student?id=12345
-# Returns: name, course, year, gpa
-# ------------------------------------------------------------
+
+
 @app.route("/api/student", methods=["GET"])
 def get_student_by_id():
     student_id = request.args.get("id")
@@ -36,11 +29,6 @@ def get_student_by_id():
     }
     return jsonify(result)
 
-
-# ------------------------------------------------------------
-# /api/students/count
-# Returns: total number of students (mock data)
-# ------------------------------------------------------------
 @app.route("/api/students/count", methods=["GET"])
 def get_students_count():
     total = len(students)
